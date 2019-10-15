fa7675cf3d287d4556927a820fe6417a2d30c3f9
Shane <shanemcneil19@gmail.com>
2019-10-14 20:13:35 -0700

    complete book search server and app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(morgan('common')); // let's see what 'common' format looks like
 
@@ -9,14 +12,28 @@ const books = require('./books-data.js');
 
 app.get('/books', (req, res) => {
   // ALL OUR CODE HERE
-    const { search = ""} = req.query;
+  const { search = "", sort } = req.query;
 
-    let results = books
+  if (sort) {
+    if (!['title', 'rank'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of title or rank');
+    }
+  } 
+
+  let results = books
         .filter(book =>
-            book
-                .title
-                .toLowerCase()
-                .includes(search.toLowerCase()));
+          book
+            .title
+            .toLowerCase()
+            .includes(search.toLowerCase()));
+        
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
 
   res.json(results);
 });
